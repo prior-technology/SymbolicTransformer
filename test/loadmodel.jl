4063f00f00c62c1c03c824286a0bae2a3873fbcf
Stephen Prior <stprior@users.noreply.github.com>
2024-04-12 22:53:36 +0100

    test refactor 2

--- test/loadmodel.jl
+++ test/loadmodel.jl
@@ -1,20 +1,24 @@
 module TestData
-using Transformers
-using Transformers.HuggingFace
-using Transformers.TextEncoders
 
-const encoder = hgf"EleutherAI/pythia-14m:tokenizer"
-const model = hgf"EleutherAI/pythia-14m:forcausallm"
+using Memoization
+import Transformers.HuggingFace: load_config,load_model,load_tokenizer
 
-function get_encoder()
-    return encoder
+const model = "EleutherAI/pythia-14m"
+
+@memoize function get_config()
+    return load_config(model)
 end
-function get_model()
-    return model
+@memoize function get_encoder()
+    config = load_config(model)
+    return load_tokenizer(model, config=config)
+end
+@memoize function get_model()
+    config = load_config(model)
+    return load_model(config.model_type, model, "forcausallm"; config=config)
 end
 
-function get_both()
-    return model, encoder
+@memoize function get_both()
+    return (get_model(), get_encoder())
 end
 
 end
